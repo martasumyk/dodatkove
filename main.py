e260f9228782e69819c7048dc32ce74c08309527
martasumyk <116710765+martasumyk@users.noreply.github.com>
2023-04-11 09:04:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ class Person:
         '''
         return f'Hello, {self.name}!'
 
+
 class Student(Person):
     '''
     Class to create a student.
@@ -28,24 +29,34 @@ class Student(Person):
         '''
         super().__init__(name, surname)
         self.subjects = dict()
+        self.talons = 0
 
     def test_talon(self):
         '''
-        Tests it person has talons.
+        Tests if person has talons.
         '''
-        talons = 0
         for i in self.subjects.items():
             if i[1] < 60:
-                talons += 1
-        if talons == 0:
+                self.talons += 1
+        if self.talons == 0:
             return f'Congratulations! {self.name} closed all the subjects!'
-        return f'{self.name} has {talons} talons.'
+        return f'{self.name} has {self.talons} talons.'
 
     def add_subject(self, subject, grade=0):
         '''
         Adds subject to the student`s list.
         '''
         self.subjects[subject] = grade
+
+    def add_task(self, task):
+        '''
+        Adds the task to a student - if it`s correct student gain points.
+        '''
+        if task.correct:
+            self.subjects[task.subject] += task.weight
+        else:
+            return f'The task was done incorrect.'
+
 
 class Task:
     '''
@@ -65,5 +76,35 @@ class Task:
         '''
         if correct:
             self.correct = True
+
+
+class Group:
+    '''
+    Class to create group of students.
+    '''
+    def __init__(self):
+        '''
+        Initializes the data.
+        '''
+        #At first group is an empty list:
+        self.students = []
+
+    def add_student(self, student: Student):
+        '''
+        Method that allows adding student to the group list.
+        '''
+        if isinstance(student, Student):
+            self.students.append(student)
+
+    def delete_students(self):
+        '''
+        Method to check if we have students that are going to be fired.
+        '''
+        if self.students:
+            for student in self.students:
+                student.test_talon()
+                if student.talons >= 3:
+                    self.students.remove(student)
+        return f'We have {len(self.students)} students left.'
 
 
