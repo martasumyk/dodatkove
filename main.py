3025976b10e7a536fb0d7acf9ef29b3a7164669b
Victor Muryn <muryn.pn@ucu.edu.ua>
2023-04-15 14:24:21 +0300

    added static/property

--- main.py
+++ main.py
@@ -12,59 +12,37 @@ class Person:
         self.name = name
         self.surname = surname
 
+    @property
+    def fullname(self) -> str:
+        ''' get persons full name '''
+        return f"{self.name} {self.surname}"
+
+    @fullname.setter
+    def fullname(self, value: str):
+        if not isinstance(value, str):
+            raise TypeError("Fullname must be str")
+
+        if len(value.split(' ')) != 2:
+            raise ValueError("Fullname must consist of 2 words: name and surname")
+
+        self.name, self.surname = value.split(' ')
+
     def __str__(self):
         '''
         Returns the string to greet with the person.
         '''
-        return f'Hello, {self.name}!'
+        return f'Hello, {self.fullname}!'
 
 class Program:
     '''
     Class of Program
     '''
-    def __init__(self, name, subjects : dict) -> None:
+    def __init__(self, name: str, subjects: list) -> None:
         self.name = name
         self.subjects = subjects
 
-
-class Student(Person):
-    '''
-    Class to create a student.
-    '''
-    def __init__(self, name, surname, program : Program, talons = 0):
-        '''
-        Initialize the data.
-        '''
-        super().__init__(name, surname)
-        self.program = program
-        self.talons = talons
-
-    def test_talon(self):
-        '''
-        Tests if person has talons.
-        '''
-        for i in self.program.subjects.items():
-            if i[1] < 60:
-                self.talons += 1
-        if self.talons == 0:
-            return f'Congratulations! {self.name} closed all the subjects!'
-        return f'{self.name} has {self.talons} talons.'
-
-    def add_subject(self, subject, grade=0):
-        '''
-        Adds subject to the student`s list.
-        '''
-        self.program.subjects[subject] = grade
-
-    def add_task(self, task):
-        '''
-        Adds the task to a student - if it`s correct student gain points.
-        '''
-        if task.correct:
-            self.program.subjects[task.subject] += task.weight
-        else:
-            return f'The task was done incorrect.'
-
+    def __str__(self) -> str:
+        return f"{self.name} program has such subjects: {self.subjects}."
 
 class Task:
     '''
@@ -85,6 +63,108 @@ class Task:
         if correct:
             self.correct = True
 
+class Grades(dict):
+    '''
+    Create grades for 1 student
+    '''
+    def __init__(self, program: Program) -> None:
+        super().__init__({subject: 0 for subject in program.subjects})
+
+    def set_grade(self, subject: str, grade: int):
+        '''
+        Set grade for student
+        '''
+        self[subject] = grade
+
+    def get_grade(self, subject: str):
+        '''
+        Get grade for subject
+        '''
+        if subject in self:
+            return self[subject]
+
+        raise ValueError(f"There's no subject {subject}")
+
+    def add_task(self, task: Task):
+        '''
+        Add task for subject
+        '''
+        self[task.subject] += task.weight
+
+
+    @staticmethod
+    def to_ECTS_grading(grade: int) -> str:
+        '''
+        Convert 1-100 grade to A-F
+        '''
+        if not 0 <= grade <= 100:
+            raise ValueError("Grade must be 0 <= grade <= 100")
+
+        if grade >= 90:
+            return "A"
+
+        if grade >= 85:
+            return "B"
+
+        if grade >= 75:
+            return "C"
+
+        if grade >= 65:
+            return "D"
+
+        if grade >= 60:
+            return "E"
+
+        return "F"
+
+
+class Student(Person):
+    '''
+    Class to create a student.
+    '''
+    def __init__(self, name, surname, program: Program, talons = 0):
+        '''
+        Initialize the data.
+        '''
+        super().__init__(name, surname)
+        self.program = program
+        self.grades = Grades(program)
+        self.talons = talons
+
+    def test_talon(self):
+        '''
+        Tests if person has talons.
+        '''
+        for grade in self.grades.values():
+            if grade < 60:
+                self.talons += 1
+
+        if self.talons == 0:
+            return f'Congratulations! {self.name} closed all the subjects!'
+
+        return f'{self.name} has {self.talons} talons.'
+
+    def add_subject(self, subject, grade = 0):
+        '''
+        Adds subject to the student`s list.
+        '''
+        self.grades.set_grade(subject, grade)
+
+    def add_task(self, task):
+        '''
+        Adds the task to a student - if it`s correct student gain points.
+        '''
+        if task.correct:
+            self.grades.add_task(task)
+        else:
+            return 'The task was done incorrect.'
+
+    def get_ECTS_grades(self):
+        '''
+        convert student grades to ETCS grading system
+        '''
+        return {subject: Grades.to_ECTS_grading(grade) for (subject, grade) in self.grades.items()}
+
 
 class Group:
     '''
@@ -94,7 +174,7 @@ class Group:
         '''
         Initializes the data.
         '''
-        #At first group is an empty list:
+        # At first group is an empty list:
         self.students = []
 
     def add_student(self, student: Student):
@@ -111,6 +191,8 @@ class Group:
         if self.students:
             for student in self.students:
                 student.test_talon()
+
                 if student.talons >= 3:
                     self.students.remove(student)
+
         return f'We have {len(self.students)} students left.'
--- tests.py
+++ tests.py
@@ -8,47 +8,72 @@ def test_StudentsClass():
     Tests classes.
     '''
     print('Testing UCU classes...')
-    #First we'll test simple class to create person:
+    # First we'll test simple class to create person:
     person = Person('Bohdan', 'Borkivskyy')
-    assert str(person) == 'Hello, Bohdan!'
 
-    #Let's create some programs!
-    #All subjects have points 0 by default:
-    cs = Program('CS', {'Discrete Math' : 0, 'Math Analysis' : 0, 'OP' : 0})
-    ba = Program('BA', {'Econom analysis' : 0, 'Math Analysis' : 0, 'OP' : 0})
+    assert person.fullname == "Bohdan Borkivskyy"
+    assert str(person) == 'Hello, Bohdan Borkivskyy!'
+
+    # Let's create some programs!
+    # All subjects have points 0 by default:
+    cs = Program('CS', ['Discrete Math', 'Math Analysis', 'OP'])
+    ba = Program('BA', ['Econom analysis', 'Math Analysis', 'OP'])
 
     assert str(ba) == "BA program has such subjects: ['Econom analysis', 'Math Analysis', 'OP']."
 
-    #Than let`s create a student - a person, who also has subjects of different program:
+    # Than let`s create a student - a person, who also has subjects of different program:
     student1 = Student('Marta', 'Sumyk', cs)
 
-    assert isinstance(student1, Person) == True
-    assert isinstance(student1, Student) == True
-    #Now let`s add some subjects and grades:
-    student1.program.subjects['OP'] = 80
-    student1.program.subjects['Discrete Math'] = 59
+    assert isinstance(student1, Person)
+    assert isinstance(student1, Student)
 
-    #Also we can add subjects that are not from a program`s list:
-    student1.program.subjects['History'] = 55
-    assert len(student1.program.subjects) == 4
-    #Now we will test if student has talons:
+    # Let's create class for grades
+    grades = Grades(cs)
+    assert grades == {'Discrete Math': 0, 'Math Analysis': 0, 'OP': 0}
+
+
+    # 90 <= A < 100
+    # 85 <= B < 80
+    # 75 <= C < 85
+    # 65 <= D < 75
+    # 60 <= E < 65
+    # 0 <= F < 60
+    assert Grades.to_ECTS_grading(70) == 'D'
+
+
+    # Now let`s add some subjects and grades:
+    student1.grades.set_grade('OP', 96)
+    student1.grades.set_grade('Discrete Math', 59)
+
+    assert student1.grades.get_grade('OP') == 96
+    assert student1.get_ECTS_grades() == {'Discrete Math': 'F', 'Math Analysis': 'F', 'OP': 'A'}
+
+    # Also we can add subjects that are not from a program`s list:
+    student1.grades.set_grade('History', 55)
+    assert len(student1.grades) == 4
+
+    # Now we will test if student has talons:
     assert student1.test_talon() == 'Marta has 3 talons.'
 
-    #Also we can add subjects to the student`s list:
+    # Also we can add subjects to the student`s list:
     student1.add_subject('Svitoglyadne yadro', 30)
-    assert len(student1.program.subjects) == 5
+    assert len(student1.grades) == 5
 
     op_task = Task('OP', 4)
     op_task.check_task(True)
     student1.add_task(op_task)
 
-    assert student1.program.subjects[op_task.subject] == 84
+    assert student1.grades.get_grade(op_task.subject) == 100
+
     math_analysis_task = Task('Math Analysis', 2)
     math_analysis_task.check_task(False)
     assert student1.add_task(math_analysis_task) == 'The task was done incorrect.'
 
     cs_group = Group()
     student2 = Student('Nazar', 'secret_surname', cs, 3)
+
+    assert student2.grades == {'Discrete Math': 0, 'Math Analysis': 0, 'OP': 0}
+
     student3 = Student('Kostya', 'secret_surname', cs, 3)
     student4 = Student('Bohdan', 'secret_surname', cs, 4)
     student5 = Student('Vlad', 'secret_surname', cs, 2)
@@ -65,9 +90,10 @@ def test_StudentsClass():
     cs_group.add_student(student7)
     cs_group.add_student(student8)
 
+
     assert len(cs_group.students) == 8
 
-    #By method 'delete_students()' we can delete students that have 3 or more talons:
+    # By method 'delete_students()' we can delete students that have 3 or more talons:
     cs_group.delete_students()
     assert len(cs_group.students) == 4
     print('Done!')
